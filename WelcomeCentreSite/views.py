1dd0d67558c0780877eb0809d8f9de8e80123593
Puzanovim <puzanovim@yandex.ru>
2021-01-21 22:41:41 +0400

    Добавил обработчик формы новой заявки

--- WelcomeCentreSite/views.py
+++ WelcomeCentreSite/views.py
@@ -1,5 +1,7 @@
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
+from pyramid_sqlalchemy import Session
+import datetime
 
 from .models import Event, Project, Order
 
@@ -72,11 +74,29 @@ def add_order(request):
     Форма заказа
     """
     if 'form.submitted' in request.params:
-        order_id = request.params['body']
-        # TODO создаем запрос
-        next_url = request.route_url('order', id=order_id)
+        date = request.params['date']
+        date = date.split("-")
+        time = request.params['time']
+        time = time.split(":")
+        order = Order(name=request.params['name'],
+                      phone=request.params['phone'],
+                      email=request.params['email'],
+                      contact_face=request.params['contact_face'],
+                      event=int(request.params['event']),
+                      date=datetime.date(year=int(date[0]),
+                                         month=int(date[1]),
+                                         day=int(date[2])),
+                      time=datetime.time(hour=int(time[0]),
+                                         minute=int(time[1])),
+                      address=request.params['address'],
+                      count_participants=int(request.params['count_participants']),
+                      note=request.params['note'])
+        Session.add(order)
+        current_order_id = Order.get_last_order().id
+        next_url = request.route_url('order', id=current_order_id)
         return HTTPFound(location=next_url)
     return {
         "title": "Добавление новой записи",
-        "save_url": request.route_url('new_order')
+        "save_url": request.route_url('new_order'),
+        "events": Event.get_events()
     }
